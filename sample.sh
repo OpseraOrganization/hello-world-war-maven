84d9b87dc86db90c9dc79f8d7b30553d15199fac
Faseeh <faseeh@opsera.io>
2021-08-03 16:50:15 +0530

    copied files from github

---
+++ sample.sh
@@ -0,0 +1,9 @@
+export PURUS_VAR1=${opsera-subnet_ids_list1}
+export PURUS_VAR2=${opsera-region}
+export PURUS_VAR3=${opsera-hellonewtest}
+export PIPELINEID=${opsera_pipeline_id}
+
+echo ${PURUS_VAR1}
+echo ${PURUS_VAR2}
+echo ${PURUS_VAR3}
+echo ${PIPELINEID}
---
+++ scripts/sample.sh
@@ -0,0 +1,19 @@
+export PURUS_VAR1=${opsera-subnet_ids_list1}
+export PURUS_VAR2=${opsera-region}
+export PURUS_VAR3=${opsera-hellonewtest}
+export PURUS_VAR4=${opsera-kafkaconnect-sink-restart}
+export PURUS_VAR5=${opsera-demo-region-hard}
+export PURUS_VAR6=${opsera-special}
+export PIPELINEID=${opsera_pipeline_id}
+
+
+echo "STEPS = $STEPS"
+echo "REPOSITORY_NAME = $REPOSITORY_NAME"
+
+echo ${PURUS_VAR1}
+echo ${PURUS_VAR2}
+echo ${PURUS_VAR3}
+echo ${PURUS_VAR4}
+echo ${PURUS_VAR5}
+echo ${PURUS_VAR6}
+echo ${PIPELINEID}
