585d4a59c6811b28b534bf4d3eebc3fb669080f0
Faseeh <faseeh@opsera.io>
2021-09-07 19:12:16 +0530

    testcommit

--- sample.sh
+++ sample.sh
@@ -4,6 +4,7 @@ export PURUS_VAR3=${opsera-hellonewtest}
 export PIPELINEID=${opsera_pipeline_id}
 
 echo ${PURUS_VAR1}
-echo ${PURUS_VAR2}
-echo ${PURUS_VAR3}
-echo ${PIPELINEID}
+echo ${PURUS_VAR1}
+# echo ${PURUS_VAR2}
+# echo ${PURUS_VAR3}
+# echo ${PIPELINEID}
